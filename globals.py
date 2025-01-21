d3f278b92d4144960b92934da94463a9aa40cf34
Morgan Borjigin-Wang <mbwang10@gmail.com>
2025-01-20 21:04:33 -0600

    Update eligible features to include mistral-summarized text

--- globals.py
+++ globals.py
@@ -9,13 +9,13 @@ CLINICAL_TSV_PATH = os.path.join(DATA_PATH, "clinical.tsv")
 
 ELIGIBLE_MODEL_TYPES = ["cox", "ipcr", "rf", "gb", "est", "cgb"]
 FEATURE_ID_DELIM = "-"
-ELIGIBLE_FEATURE_TYPES = frozenset(["project", "demo", "cancer", "expr", "rawexpr", "text", "text_sum_bm", "text_sum_co", "hist_mean", "hist_max"])
+ELIGIBLE_FEATURE_TYPES = frozenset(["project", "demo", "cancer", "expr", "rawexpr", "text", "text_sum_bm", "text_sum_m", "text_sum_co", "hist_mean", "hist_max"])
 # Thee feature types have duplicated data somehow and shouldn't be used together
 # TODO: Maybe using them together wouldn't be a bad idea...? Something interesting to try
 INCOMPATIBLE_FEATURE_TYPES = [
     frozenset(["expr", "rawexpr"]),
     frozenset(["hist_mean", "hist_max"]),
-    frozenset(["text", "text_sum_bm", "text_sum_co"]),
+    frozenset(["text", "text_sum_bm", "text_sum_m", "text_sum_co"]),
 ]
 for ft in ELIGIBLE_FEATURE_TYPES:
     assert FEATURE_ID_DELIM not in ft
